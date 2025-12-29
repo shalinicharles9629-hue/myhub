f83f7264adddbc9e754cb0ae16fe66befcda9696
shalinicharles9629-hue <shalinicharles9629@gmail.com>
2025-12-29 12:37:29 +0530

    welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useState } from 'react';
 import './App.css'
 import About from './component/About'
 import Contact from './component/Contact'
 import Home from'./component/Home'
 import Services from'./component/Services'
+import Use from './component/use'
 import { Route,Link, Routes } from 'react-router-dom';
 function App() {
    
@@ -15,12 +15,14 @@ function App() {
       <Link to="/about">About</Link>
       <Link to="/Contact">Contact</Link>
       <Link to="/Services">Services</Link>
+      <Link to="/Use">Use</Link>
     </nav>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/Contact" element={<Contact/>} />
       <Route path="/Services" element={<Services/>} />
+      <Route path="/use" element={<Use/>} />
     </Routes>
     </>
     
---
+++ src/component/use.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from "react"
+function Counter(){
+    const[count,setCount]=useState(0);
+    return(
+        <div>
+            <p>You Clicked {count} times</p>
+            <button onClick={()=>setCount(count+1)}>Click me</button>
+        </div>
+    )
+}
+export default Counter;
